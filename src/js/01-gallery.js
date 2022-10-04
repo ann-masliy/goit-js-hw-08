3c130e993f28a933cb5a13a699d1971f7073698d
ann-masliy <ms.masliy@ukr.net>
2022-10-04 12:16:33 +0300

    add SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,8 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox'
 import { galleryItems } from './gallery-items';
+
+console.log(SimpleLightbox);
 // Change code below this line
 
 console.log(galleryItems);
