d7edccc432b2e487ec3d98f331f16b4a8d632e06
ann-masliy <ms.masliy@ukr.net>
2022-10-04 12:50:23 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,23 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox'
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(SimpleLightbox);
 // Change code below this line
+const gallery = document.querySelector('.gallery')
+const galleryItem = galleryItems.map(galleryItem => 
+    `<a class="gallery__item" href="${galleryItem.original}">
+<img
+    class="gallery__image"
+    src="${galleryItem.preview}"
+    data-source= "${galleryItem.original}"
+    alt="${galleryItem.description}"
+/>
+</a>`)
+.join("");
+
+gallery.innerHTML = galleryItem;
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
 
 console.log(galleryItems);
